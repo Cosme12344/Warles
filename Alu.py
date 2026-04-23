a6be11a16a644355b664a65f813df9f4eed1055b
Cosme12344 <cosmedamiaopires929@gmail.com>
2026-04-23 07:29:39 -0300

    Create Alu.py

---
+++ Alu.py
@@ -0,0 +1,14 @@
+with open("aluno.txt", "w", encoding="utf-8") as arquivo:
+    arquivo.write("ID/Nome/Idade/Turma/Nota\n")
+    arquivo.write("1/Lucas/14/1B/7.8\n")
+    arquivo.write("2/Mariana/17/2A/8.9\n")
+    arquivo.write("3/Pedro/16/2C/6.7\n")
+    arquivo.write("4/Beatriz/15/1A/9.3\n")
+    arquivo.write("5/Rafael/18/3A/7.2\n")
+    arquivo.write("6/Camila/16/2B/8.4\n")
+    arquivo.write("7/Thiago/17/3B/6.5\n")
+    arquivo.write("8/Isabela/15/1C/9.0\n")
+    arquivo.write("9/André/14/1A/7.1\n")
+    arquivo.write("10/Carolina/18/3C/8.6\n")
+
+print("Arquivo aluno.txt criado com sucesso!")
